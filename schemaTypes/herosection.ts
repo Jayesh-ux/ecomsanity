f4550b7ad69c939dd5796a5ab7d0a7a51db60dfc
Jayesh Singh <hsinghjayesh@gmail.com>
2024-06-16 13:32:09 +0530

    committed

---
+++ schemaTypes/herosection.ts
@@ -0,0 +1,22 @@
+export default {
+    name: 'herosection',
+    title: 'Hero Section',
+    type: 'document',
+    fields: [
+      {
+        name: 'heroText',
+        title: 'Hero Text',
+        type: 'string',
+      },
+      {
+        name: 'buttonText',
+        title: 'Button Text',
+        type: 'string',
+      },
+      {
+        name: 'buttonLink',
+        title: 'Button Link',
+        type: 'url',
+      },
+    ],
+  }
---
+++ schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import category from "./category";
+import heroimage from "./heroimage";
+import herosection from "./herosection";
+import product from "./product";
+
+
+export const schemaTypes = [product , category, heroimage, herosection,]
